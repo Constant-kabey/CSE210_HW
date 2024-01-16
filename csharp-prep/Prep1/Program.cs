ac2adbb3ce0e06c1b97b337e958628010688c6c9
kabeyconstant <kabeyconstant@gmail.com>
2024-01-16 05:37:06 -0800

    Finish_Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.Write("What is your first name?");
+        string FirstName = Console.ReadLine();
+        Console.Write("What is your family name?");
+        string FamName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {FamName}, {FirstName} {FamName}");
+
     }
 }
