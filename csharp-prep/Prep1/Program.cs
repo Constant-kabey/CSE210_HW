0b23fcbbf9bc0d04a71d1e5b8ec9d971a81d7bdd
kabeyconstant <kabeyconstant@gmail.com>
2024-01-16 05:52:32 -0800

    Condition updates

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -13,6 +13,44 @@ class Program
 
         Console.WriteLine($"Your name is {FamName}, {FirstName} {FamName}");
 
+        Console.Write("What is your pourcentage? ");
+        string pourcentage = Console.ReadLine();
+        int percent = int.Parse(pourcentage);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Low score, please, try to work hard!");
+        }
+
 
     }
 }
